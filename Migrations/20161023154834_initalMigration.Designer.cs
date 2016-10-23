403f363adc5f40e93a430926fdefa38ae77dab13
Pon ArunKumar Ramalingam <ponarunkumar@gmail.com>
2016-10-23 12:05:36 -0400

    Console Blog App dotnetcore SQLite Sample

---
+++ Migrations/20161023154834_initalMigration.Designer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ConsoleBlogApp.SQLite;
+
+namespace ConsoleBlogApp.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    [Migration("20161023154834_initalMigration")]
+    partial class initalMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ChangeDetector.SkipDetectChanges", "true")
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleBlogApp.SQLite
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var db = new BloggingContext()) {
+                db.Blogs.Add(new Blog {Url = "http://blogs.msdn.com/adonet"});
+                var count = db.SaveChanges();
+                Console.WriteLine($"{count} records saved to database");
+                Console.WriteLine();
+                Console.WriteLine("All blogs in SQLite Database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine($" - {blog.Url}");
+                }
+}       }
+    }
+}
